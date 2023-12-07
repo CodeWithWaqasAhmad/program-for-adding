81e953c57be89aafb6508cd571af4ce8486b9c05
Waqas Ahmad <153016958+CodeWithWaqasAhmad@users.noreply.github.com>
2023-12-06 22:36:55 -0700

    Add files via upload
    simple addition program

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number2,number3,result;
+
+    cout>>"enter your second number" <<endl;
+    cin>>number2;
+
+    cout>>"enter your third number" <<endl;
+    cin>>number3;
+
+    result = number2 + number3;
+    cout>>"the sum is equal to";
+
+
+    return 0;
+}
